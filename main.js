6346d6396b8b693a67fa511e031cd4a5402106aa
PeddiMeenakshi <peddimeenakshi17@gmail.com>
2025-05-27 05:18:08 +0530

    Syntax, Data Types, and Operators @JS

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ console.log("Welcome to the Community Portal");
 window.onload = function() {
     alert("Page is fully loaded!");
 };
+const eventName = "Marathon";
+const eventDate = "June 15, 2025";
+let availableSeats = 500;
+const eventDetails = `Event: ${eventName}\nDate: ${eventDate}\nAvailable Seats: ${availableSeats}`;
+console.log(eventDetails);
+function registerAttendee() {
+    if (availableSeats > 0) {
+        availableSeats--;
+        console.log(`Registration successful! Seats remaining: ${availableSeats}`);
+    } else {
+        console.log("Sorry, no seats available.");
+    }
+}
